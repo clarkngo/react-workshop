38b6ea97fad0f57721c7400cb054675c2cd3eb76
Clark Ngo <clarkngo@gmail.com>
2022-01-23 23:21:18 -0800

    exercise 1.03

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import React from 'react';
-const App = props => (
-  <div>
-    <h1>My App</h1>
-    <br />
-    Hello React!
-  </div>
-)
+import React, {Component} from 'react';
+
+const Greeting = props => <p>Hello {props.name}!</p>;
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.title = 'React App';
+    this.state = { clickCounter: 0};
+  }
+
+  renderClickcount() {
+    return <p>I've been clicked {this.state.clickCounter} times!</p>
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>My App</h1>
+        <h1>{this.title}</h1>
+        <Greeting name="User" />
+        {this.renderClickcount()}
+      </div>
+    )
+  }
+}
+
 export default App;
