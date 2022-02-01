7fbd0c9c4e948a2d415835195f434e01283e9b8d
Clark Ngo <clarkngo@gmail.com>
2022-02-01 01:22:57 -0800

    textarea character count

--- src/App.js
+++ src/App.js
@@ -6,96 +6,43 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = { 
-      username: '',
-      password: '',
-      passwordConfirmation: '',
-      email: '',
-      errors: []
+      fieldLength: 0,
     };
-    this.validateUsernameOnBlur = this.validateUsernameOnBlur.bind(this);
-    this.validatePasswordOnBlur = this.validatePasswordOnBlur.bind(this);
-    this.validatePasswordConfirmationOnBlur = this.validatePasswordConfirmationOnBlur.bind(this);
-    this.validateEmailOnBlur = this.validateEmailOnBlur.bind(this);
+    this.updateFieldLength = this.updateFieldLength.bind(this);
+  }
+
+  updateFieldLength = (event) => {
+    const fieldLength = event.target.value.length;
+    this.setState({ fieldLength });
+  }
+
+  validateTextLength = (event) => {
+    const text = event.target.value;
+    if (text.length < 100) {
+      console.log("character count must be 100 or more");
+    }
   }
 
   displayForm() {
     return (
       <div>
-        Username: <input type="text" onBlur={this.validateUsernameOnBlur} /><br />
-        Password: <input type="text" onBlur={this.validatePasswordOnBlur} /><br />
-        Password Confirmation: <input type="text" onBlur={this.validatePasswordConfirmationOnBlur}/><br />
-        Email: <input type="text" onBlur={this.validateEmailOnBlur} /><br />
-        <br />
+        <textarea onChange={this.updateFieldLength} onBlur={this.validateTextLength}></textarea>
         <button onClick={this.submitForm}>Submit</button>
-      </div>
-    )
-  }
-
-  displayErrors() {
-    return (
-      <div className='errors'>
-        {this.state.errors.map((err, i) => <p key={`err-${i}`}>{err}</p>)}
+        <p>Character count: {this.state.fieldLength}</p>
       </div>
     );
   }
 
-  validateUsernameOnBlur = (event) => {
-    const username = event.target.value;
-    const errors = this.state.errors;
-    errors.push(this.validateNotEmpty("Username", username));
-    this.setState({ username, errors });
-  }
-
-  validatePasswordOnBlur = (event) => {
-    const password = event.target.value;
-    const errors = this.state.errors;
-    errors.push(this.validateNotEmpty("Password", password));
-    this.setState({ password, errors });
-  }
-
-  validatePasswordConfirmationOnBlur = (event) => {
-    const passwordConfirmation = event.target.value;
-    const errors = this.state.errors;
-    if (passwordConfirmation != this.state.password) {
-      errors.push("Password must match password confirmation");
-    }
-    this.setState({ passwordConfirmation, errors });
-  }
-
-  validateEmailOnBlur = (event) => {
-    const email = event.target.value;
-    const errors = this.state.errors;
-    errors.push(this.validateEmailFormat("Email", email));
-    this.setState({ email, errors });
-  }
-
-  validateNotEmpty(fieldName, value) {
-    if (value.length <= 0) {
-      return `${fieldName} must be filled out.`;
-    }
-  }
-
-  validateEmailFormat(fieldName, value) {
-    let [lhs, rhs] = value.split('@');
-    lhs = lhs || '';
-    rhs = rhs || '';
-    if (lhs.length <= 0 || rhs.length <= 0) {
-      return `${fieldName} must be in a standard email format.`;
-    }
-  }
-
   submitForm(event) {
     event.preventDefault();
-    console.log("Submitting form now..");
-    console.log(event);
   }
 
   render() {
     return (
-      <div className="App">
-        Create Account
-        {this.displayErrors()}
+      <div>
+        <h1>Blog Post Writer</h1>
         <hr />
+        <h2>Write your post here</h2>
         {this.displayForm()}
       </div>
     );
